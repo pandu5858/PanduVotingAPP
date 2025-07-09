cd2c6b59b5be4eeb8f281a516de84aa10eadbc79
pandu5858 <pandukadiyala170@gmail.com>
2025-07-09 23:55:34 +0530

    Initial commit for Render deployment

--- MySpring_Boot_aa23v_VotingApp_Final/src/main/java/com/model/Candidate.java
+++ MySpring_Boot_aa23v_VotingApp_Final/src/main/java/com/model/Candidate.java
@@ -27,6 +27,24 @@ public class Candidate {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCandidate() {
+		return candidate;
+	}
+	public void setCandidate(String candidate) {
+		this.candidate = candidate;
+	}
+	public int getVotes() {
+		return votes;
+	}
+	public void setVotes(int votes) {
+		this.votes = votes;
+	}
 	@Column(unique = true)
 	private String candidate;
 	private int votes;
